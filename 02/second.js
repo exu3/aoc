b3f3392ae95ab6ed2d6b48598fc721d785046884
eilla1 <git@ella.cx>
2021-12-01 22:18:07 -0800

    Wahoo. Part 2.

---
+++ 02/second.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf8").split("\n");
+
+let x = 0;
+let y = 0;
+let aim = 0;
+
+const nestedArr = [];
+
+for (let j = 0; j < input.length; j++) {
+  let object = input[j].split(" ");
+  nestedArr.push(object);
+}
+
+function move() {
+  for (let i = 0; i < nestedArr.length; i++) {
+    const direction = nestedArr[i][0];
+    const units = parseInt(nestedArr[i][1], 10);
+    if (direction == "forward") {
+      y += aim * units;
+      x += units;
+      console.log("depth is " + y);
+    } else if (direction == "up") {
+      //   y -= units;
+      aim -= units;
+      console.log("depth is " + y);
+    } else if (direction == "down") {
+      //   y += units;
+      aim += units;
+      console.log("depth " + y);
+    } else {
+      console.log("hmmmm idk");
+    }
+  }
+
+  console.log(x);
+  console.log(y);
+  console.log(x * y);
+}
+
+move();
